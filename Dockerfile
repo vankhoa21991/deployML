39d4a8e9d63836e85d84727815d5ee36217eefd4
vankhoa21991 <vankhoa21991@gmail.com>
2024-05-09 22:33:28 +0200

    finish streamlit app

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,12 @@
-# Use the official Python image as a base image
 FROM python:3.9-slim
 
-# Set the working directory in the container
 WORKDIR /app
 
-# Copy the current directory contents into the container at /app
-COPY . /app
-
-# Install dependencies
+COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Expose port 5000 to the outside world
-EXPOSE 5000
+COPY . .
 
-# Define environment variable
-ENV NAME World
+EXPOSE 8501
 
-# Command to run the Flask application
-CMD ["python", "app.py"]
+ENTRYPOINT ["streamlit", "run", "app.py", "--server.port=8501", "--server.address=0.0.0.0"]
--- command.sh
+++ command.sh
@@ -11,6 +11,17 @@ kubectl delete deployment test-ml-score-api
 kubectl create deployment test-ml-score-api --image=loup21991/flask-api:latest
 kubectl rollout status deployment test-ml-score-api
 kubectl get pods
+minikube service list
 kubectl port-forward test-ml-score-api-7f766c47b4-mb9fc 5000:5000
 kubectl expose deployment test-ml-score-api --port 5000 --type=LoadBalancer --name test-ml-score-api-lb
-minikube service list
+
+kubectl apply -f py-flask-ml-score-api/py-flask-ml-score.yaml
+kubectl delete -f py-flask-ml-score-api/py-flask-ml-score.yaml
+
+
+
+
+kubectl delete deployment streamlit-app
+kubectl delete service streamlit-service
+
+https://github.com/AlexIoannides/kubernetes-mlops/tree/master
